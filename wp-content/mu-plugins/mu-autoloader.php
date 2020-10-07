ad31c25a7ee6340030506dab873b5a94d4e61462
Harry Edwards <harryedwards.nc@gmail.com>
2020-10-07 15:36:36 +0100

    autoloader update

--- wp-content/mu-plugins/mu-autoloader.php
+++ wp-content/mu-plugins/mu-autoloader.php
@@ -8,3 +8,4 @@ require WPMU_PLUGIN_DIR.'/redirection/redirection.php';
 require WPMU_PLUGIN_DIR.'/timber-library/timber.php';
 require WPMU_PLUGIN_DIR.'/wordpress-seo/wp-seo.php';
 require WPMU_PLUGIN_DIR.'/wps-hide-login/wps-hide-login.php';
+require WPMU_PLUGIN_DIR.'/wp-cfm/wp-cfm.php';
